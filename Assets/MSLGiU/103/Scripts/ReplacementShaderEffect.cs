24fd8892aa70a84ae9d682f50f57dd9ef512c22f
Jesse Yeh <jesseyeh@users.noreply.github.com>
2016-08-23 12:54:02 -0700

    Format code

--- Assets/MSLGiU/103/Scripts/ReplacementShaderEffect.cs
+++ Assets/MSLGiU/103/Scripts/ReplacementShaderEffect.cs
@@ -3,27 +3,23 @@ using System.Collections;
 using System.Collections.Generic;
 
 [ExecuteInEditMode]
-public class ReplacementShaderEffect : MonoBehaviour
-{
-    public Shader ReplacementShader;
-    public Color OverDrawColor;
+public class ReplacementShaderEffect : MonoBehaviour {
+  public Shader ReplacementShader;
+  public Color OverDrawColor;
 
-    // called when script is loaded or value is changed in the inspector
-    void OnValidate()
-    {
-        Shader.SetGlobalColor("_OverDrawColor", OverDrawColor);
-    }
+  // called when script is loaded or value is changed in the inspector
+  void OnValidate() {
+    Shader.SetGlobalColor("_OverDrawColor", OverDrawColor);
+  }
 
-    void OnEnable()
-    {
-        if (ReplacementShader != null)
-            // passing in "" for the tag will only apply the first subshader
-            // in the case of OverDraw, we want everything to use the same subshader
-            GetComponent<Camera>().SetReplacementShader(ReplacementShader, "");
-    }
+  void OnEnable() {
+    if(ReplacementShader != null)
+      // passing in "" for the tag will only apply the first subshader
+      // in the case of OverDraw, we want everything to use the same subshader
+      GetComponent<Camera>().SetReplacementShader(ReplacementShader, "");
+  }
 
-    void OnDisable()
-    {
-        GetComponent<Camera>().ResetReplacementShader();
-    }
+  void OnDisable() {
+    GetComponent<Camera>().ResetReplacementShader();
+  }
 }
