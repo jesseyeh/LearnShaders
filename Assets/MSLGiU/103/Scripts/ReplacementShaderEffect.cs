2a476f47e672243a74e41a7d8825d8539320f4ae
Jesse Yeh <jesseyeh@users.noreply.github.com>
2016-08-23 12:37:03 -0700

    Shaders 103

---
+++ Assets/MSLGiU/103/Scripts/ReplacementShaderEffect.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+[ExecuteInEditMode]
+public class ReplacementShaderEffect : MonoBehaviour
+{
+    public Shader ReplacementShader;
+    public Color OverDrawColor;
+
+    // called when script is loaded or value is changed in the inspector
+    void OnValidate()
+    {
+        Shader.SetGlobalColor("_OverDrawColor", OverDrawColor);
+    }
+
+    void OnEnable()
+    {
+        if (ReplacementShader != null)
+            // passing in "" for the tag will only apply the first subshader
+            // in the case of OverDraw, we want everything to use the same subshader
+            GetComponent<Camera>().SetReplacementShader(ReplacementShader, "");
+    }
+
+    void OnDisable()
+    {
+        GetComponent<Camera>().ResetReplacementShader();
+    }
+}
---
+++ Assets/MSLGiU/103/Shaders/OverDraw.shader
@@ -0,0 +1,49 @@
+﻿Shader "103/OverDraw"
+{
+	SubShader
+	{
+		Tags
+		{
+      // render the replacement shader over the skybox
+			"Queue" = "Transparent"
+		}
+
+		ZTest Always  // regardless of what is in the depth buffer, this will always be drawn
+		ZWrite Off    // either ZTest Always or ZWrite Off is sufficient
+		Blend One One // additive
+
+		Pass
+		{
+			CGPROGRAM
+			#pragma vertex vert
+			#pragma fragment frag
+
+			#include "UnityCG.cginc"
+
+			struct appdata
+			{
+				float4 vertex : POSITION;
+			};
+
+			struct v2f
+			{
+				float4 vertex : SV_POSITION;
+			};
+
+			v2f vert(appdata v)
+			{
+				v2f o;
+				o.vertex = mul(UNITY_MATRIX_MVP, v.vertex);
+				return o;
+			}
+
+			half4 _OverDrawColor;
+
+			fixed4 frag(v2f i) : SV_Target
+			{
+				return _OverDrawColor;
+			}
+			ENDCG
+		}
+	}
+}
