8c6256783ef9c8cdb301697eefb4f35a638cbc43
Jesse Yeh <jesseyeh@users.noreply.github.com>
2016-09-16 00:28:12 -0700

    Case Study: Battle Transitions

---
+++ Assets/MSLGiU/CaseStudy/BattleTransitions/Scripts/SimpleBlit.cs
@@ -0,0 +1,11 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleBlit : MonoBehaviour {
+
+  public Material TransitionMaterial;
+
+  void OnRenderImage(RenderTexture src, RenderTexture dst) {
+    Graphics.Blit(src, dst, TransitionMaterial);
+  }
+}
